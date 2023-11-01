25badc3d4d4feb91b440270b0ca87ed3729980dc
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-11-01 16:36:13 -0700

    Xs and Os rendering

--- client/src/SmallGame.jsx
+++ client/src/SmallGame.jsx
@@ -1,11 +1,53 @@
-export default function SmallGame({ boardState,  boardWidth, squareWidth }) {
+export default function SmallGame({ boardState, squareWidth }) {
     const squareBorderStyle = "thin solid black";
+
+    function squareState(stateString) {
+        switch (stateString) {
+            case 'x':
+                return (
+                    <div style={{
+                        width: squareWidth,
+                        height: squareWidth,
+                        display: 'grid',
+                        gridTemplateColumns: `${squareWidth / 2}px `.repeat(2).trim(),
+                        gridTemplateRows: `${squareWidth / 2}px `.repeat(2).trim(),
+                        transform: 'rotate(45deg)'
+                    }}>
+                        <div style={{
+                            borderRight: squareBorderStyle,
+                            borderBottom: squareBorderStyle
+                        }}></div>
+                        <div style={{
+                            borderLeft: squareBorderStyle,
+                            borderBottom: squareBorderStyle
+                        }}></div>
+                        <div style={{
+                            borderTop: squareBorderStyle,
+                            borderRight: squareBorderStyle
+                        }}></div>
+                        <div style={{
+                            borderLeft: squareBorderStyle,
+                            borderTop: squareBorderStyle
+                        }}></div>
+                    </div>
+                );
+            case 'o':
+                return (
+                    <div style={{
+                        width: squareWidth * .8,
+                        height: squareWidth * .8,
+                        border: "2px solid black",
+                        borderRadius: "50%"
+                    }}></div>
+                )
+            case 'none':
+                return null;
+        }
+    }
 
     return (
         <div style={{
             display: "grid",
-            width: {boardWidth},
-            height: {boardWidth},
             gridTemplateColumns: `${squareWidth}px `.repeat(3).trim(),
             gridTemplateRows: `${squareWidth}px `.repeat(3).trim()
         }}>
@@ -14,14 +56,14 @@ export default function SmallGame({ boardState,  boardWidth, squareWidth }) {
                 height: {squareWidth},
                 borderRight: squareBorderStyle,
                 borderBottom: squareBorderStyle
-            }}></div>
+            }}>{squareState('x')}</div>
             <div style={{
                 width: {squareWidth},
                 height: {squareWidth},
                 borderLeft: squareBorderStyle,
                 borderBottom: squareBorderStyle,
                 borderRight: squareBorderStyle
-            }}></div>
+            }}>{squareState('o')}</div>
             <div style={{
                 width: {squareWidth},
                 height: {squareWidth},
